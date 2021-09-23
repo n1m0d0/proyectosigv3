96972286a4843ddce3ed8301d83a6ac93a99858c
n1m0d0 <vascojohn@gmail.com>
2021-09-23 10:05:27 -0400

    configuracion del token de vigencia de 2 horas, payload con el id de usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,6 +73,8 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'id' => $this->id,
+        ];
     }
 }
